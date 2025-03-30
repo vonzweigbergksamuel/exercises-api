321db3de69d591ff644f56c897750af73e0a2ba1
SvZ <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-03-30 19:45:41 +0200

    Implement simple http server

---
+++ README.md
@@ -0,0 +1,4 @@
+# Exercises REST-API built in Golang
+
+> [!NOTE]
+> I'm trying to learn Golang and this is my first project with the language. With that said, this is a work in progress!
--- main.go
+++ main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := http.NewServeMux()
+	
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: middleware(router),
+	}
+
+	fmt.Printf("Server is running on http://localhost%s\n", server.Addr)
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		parts := strings.Split(r.RequestURI, "/")
+		name := "World"
+		if len(parts) > 1 && parts[1] != "" {
+			name = parts[1]
+		}
+		fmt.Fprintf(w, "Hello, %s!", name)
+	})
+
+	server.ListenAndServe()
+}
+
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Middleware")
+		next.ServeHTTP(w, r)
+	})
 }
